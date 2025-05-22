84a9f3b1453f4e3ad5fecfb464f02c50729f4c32
Aman Sachan <codewithaman78@gmail.com>
2025-05-23 04:43:18 +0530

    Added Todo Creation

--- src/main/java/com/codewithaman/todoapispring/TodoController.java
+++ src/main/java/com/codewithaman/todoapispring/TodoController.java
@@ -1,6 +1,9 @@
 package com.codewithaman.todoapispring;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,11 +11,22 @@ import java.util.List;
 
 @RestController
 public class TodoController {
-    private static List<Todo> todos;
+    private static List<Todo> todoList;
 
     public TodoController() {
-        todos = new ArrayList<>();
-        todos.add(new Todo("Go to gym", 1, false, 101));
-        todos.add(new Todo("Go to school", 2, true, 102));
+        todoList = new ArrayList<>();
+        todoList.add(new Todo("Go to gym", 1, false, 101));
+        todoList.add(new Todo("Go to school", 2, true, 102));
+    }
+
+    @GetMapping(path = "/todos")
+    public List<Todo> getAllTodos() {
+        return todoList;
+    }
+
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo) {
+        todoList.add(newTodo);
+        return newTodo;
     }
 }
