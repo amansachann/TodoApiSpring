843b32cc1763666b7fca44a506ee5d78dc190aee
Aman Sachan <codewithaman78@gmail.com>
2025-05-23 03:51:02 +0530

    Added Todo Controller

---
+++ src/main/java/com/codewithaman/todoapispring/Todo.java
@@ -0,0 +1,58 @@
+package com.codewithaman.todoapispring;
+
+public class Todo{
+	private String todo;
+	private int id;
+	private boolean completed;
+	private int userId;
+
+	public void setTodo(String todo){
+		this.todo = todo;
+	}
+
+	public String getTodo(){
+		return todo;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setCompleted(boolean completed){
+		this.completed = completed;
+	}
+
+	public boolean isCompleted(){
+		return completed;
+	}
+
+	public void setUserId(int userId){
+		this.userId = userId;
+	}
+
+	public int getUserId(){
+		return userId;
+	}
+
+	public Todo(String todo, int id, boolean completed, int userId) {
+		this.todo = todo;
+		this.id = id;
+		this.completed = completed;
+		this.userId = userId;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Todo{" + 
+			"todo = '" + todo + '\'' + 
+			",id = '" + id + '\'' + 
+			",completed = '" + completed + '\'' + 
+			",userId = '" + userId + '\'' + 
+			"}";
+		}
+}
--- src/main/java/com/codewithaman/todoapispring/TodoController.java
+++ src/main/java/com/codewithaman/todoapispring/TodoController.java
@@ -3,6 +3,16 @@ package com.codewithaman.todoapispring;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class TodoController {
+    private static List<Todo> todos;
+
+    public TodoController() {
+        todos = new ArrayList<>();
+        todos.add(new Todo("Go to gym", 1, false, 101));
+        todos.add(new Todo("Go to school", 2, true, 102));
+    }
 }
