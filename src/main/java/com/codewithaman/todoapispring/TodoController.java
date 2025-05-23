7c218304c83647c1457ab1ec68a79396a24f8e9c
Aman Sachan <codewithaman78@gmail.com>
2025-05-23 05:49:04 +0530

    Added custom message along with Not Found Status

--- src/main/java/com/codewithaman/todoapispring/TodoController.java
+++ src/main/java/com/codewithaman/todoapispring/TodoController.java
@@ -5,10 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
+@RequestMapping("/api/v1/todos")
 public class TodoController {
     private static List<Todo> todoList;
 
@@ -18,13 +19,12 @@ public class TodoController {
         todoList.add(new Todo("Go to school", 2, true, 102));
     }
 
-    @GetMapping(path = "/todos")
+    @GetMapping
     public ResponseEntity<List<Todo>> getAllTodos() {
         return ResponseEntity.ok(todoList);
     }
 
-    @PostMapping("/todos")
-
+    @PostMapping
     public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo) {
         /*
          * @ResponseStatus(HttpStatus.CREATED)
@@ -32,5 +32,25 @@ public class TodoController {
          */
         todoList.add(newTodo);
         return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
+    }
+
+    @GetMapping("/{todoId}")
+    public ResponseEntity<?> getTodoById(@PathVariable("todoId") int id) {
+        for(Todo todo: todoList) {
+            if(todo.getId() == id) return ResponseEntity.ok(todo);
+        }
+
+        /*
+         * return ResponseEntity.notFound().build();
+         * This is the simpler way to return Not Found Status
+         * without any custom message
+         */
+
+//        Return Not Found Status along with custom message
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("status", "404");
+        errorResponse.put("message", "Todo with id " + id + " not found.");
+        errorResponse.put("timestamp", String.valueOf(LocalDateTime.now()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
